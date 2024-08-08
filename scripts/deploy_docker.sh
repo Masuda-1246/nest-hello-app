21f84a30c69e2d800f1ff047dcb21c96fbb3325c
Masuda-1246 <yaojiezengtian38@gmail.com>
2024-08-08 15:48:44 +0900

    fix

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-LOGFILE=/home/ec2-user/myapp/deployment.log
+ROOT_DIR=/home/ec2-user/myapp
+LOGFILE=$ROOT_DIR/deploy.log
+
+# ディレクトリとファイルの権限を設定
+sudo chown -R ec2-user:ec2-user $ROOT_DIR
+sudo touch $LOGFILE
+sudo chown ec2-user:ec2-user $LOGFILE
+sudo chmod 644 $LOGFILE
+
 echo "Starting deployment script" > $LOGFILE
+echo "Starting deployment script"
 
 # 必要な環境変数をSSM Parameter Storeから取得して.envファイルを作成
 PARAMETER_NAMES=(
@@ -9,20 +18,45 @@ PARAMETER_NAMES=(
 )
 
 echo "Creating .env file..." >> $LOGFILE
+echo "Creating .env file..."
 
+# .envファイルをバックアップし、後で新しい内容で上書きする
+if [ -f $ROOT_DIR/.env ]; then
+  cp $ROOT_DIR/.env $ROOT_DIR/.env.bak
+fi
+
+# 新しい内容で.envファイルを作成
 for PARAM in "${PARAMETER_NAMES[@]}"; do
   KEY=$(echo $PARAM | awk -F'/' '{print $NF}')
   VALUE=$(aws ssm get-parameter --name $PARAM --with-decryption --query "Parameter.Value" --output text 2>> $LOGFILE)
   if [ $? -ne 0 ]; then
     echo "Failed to get parameter: $PARAM" >> $LOGFILE
+    echo "Restoring from backup..." >> $LOGFILE
+    if [ -f $ROOT_DIR/.env.bak ]; then
+      mv $ROOT_DIR/.env.bak $ROOT_DIR/.env
+    fi
     exit 1
   fi
-  echo "$KEY=$VALUE" >> /home/ec2-user/myapp/.env
+  # 既存の値をチェックして、なければ追加
+  if grep -q "^$KEY=" $ROOT_DIR/.env.bak; then
+    sed -i "" "s/^$KEY=.*/$KEY=$VALUE/" $ROOT_DIR/.env.bak
+  else
+    echo "$KEY=$VALUE" >> $ROOT_DIR/.env.bak
+  fi
 done
+
+# バックアップファイルを新しい.envファイルとして置き換え
+mv $ROOT_DIR/.env.bak $ROOT_DIR/.env
+
+# バックアップが不要になった場合、削除
+if [ $? -eq 0 ]; then
+  echo "Removing backup file..." >> $LOGFILE
+  rm -f $ROOT_DIR/.env.bak
+fi
 
 # GitHubリポジトリからソースコードをクローン
 echo "Pulling latest code from GitHub..." >> $LOGFILE
-cd /home/ec2-user/myapp
+cd $ROOT_DIR
 git pull origin main >> $LOGFILE 2>&1
 if [ $? -ne 0 ]; then
   echo "Failed to pull latest code from GitHub" >> $LOGFILE
@@ -47,7 +81,7 @@ fi
 
 # 新しいコンテナを実行
 echo "Running new container..." >> $LOGFILE
-docker run -d --name myapp-container --env-file /home/ec2-user/myapp/.env -p 3001:3001 myapp-image >> $LOGFILE 2>&1
+docker run -d --name myapp-container --env-file $ROOT_DIR/.env -p 80:3000 myapp-image >> $LOGFILE 2>&1
 if [ $? -ne 0 ]; then
   echo "Failed to run new container" >> $LOGFILE
   exit 1
